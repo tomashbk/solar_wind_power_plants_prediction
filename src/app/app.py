185202e7752461312f97a5621d9255b68dab8314
Tomás Haberkon <tomashaberkon@gmail.com>
2022-07-26 17:33:22 -0300

    Lots of new things added. KNN model  generated in pkl and starting to adapt app.py with the solar regressor

--- src/app/app.py
+++ src/app/app.py
@@ -16,14 +16,27 @@ def index():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    model = joblib.load(utils.DIR_MODELS/"xgbclass_model_1.pkl")
-    df_classification_y_real_values = pd.read_csv(utils.DIR_DATA_PROCESSED/"classification_y_real_values.csv", index_col=0)
-    results_lat_lon = utils.fetch_data_latitude_longitude(latitude, longitude)
-    int_prediction = int(model.predict(results_lat_lon))
-    prediction = df_classification_y_real_values.squeeze().to_list()[int_prediction]
+    if request.form.get("type") == 'classification':
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+        model = joblib.load(utils.DIR_MODELS/"xgbclass_model_1.pkl")
+        df_classification_y_real_values = pd.read_csv(utils.DIR_DATA_PROCESSED/"classification_y_real_values.csv", index_col=0)
+        results_lat_lon = utils.fetch_data_latitude_longitude(latitude, longitude)
+        int_prediction = int(model.predict(results_lat_lon))
+        prediction = df_classification_y_real_values.squeeze().to_list()[int_prediction]
+    elif request.form.get("type") == 'regression':
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+        capacity_mw = int(request.form.get("capacity_mw"))
+        # model = joblib.load(utils.DIR_MODELS/"xgbclass_model_1.pkl")
+        # df_classification_y_real_values = pd.read_csv(utils.DIR_DATA_PROCESSED/"classification_y_real_values.csv", index_col=0)
+        # results_lat_lon = utils.fetch_data_latitude_longitude(latitude, longitude)
+        # int_prediction = int(model.predict(results_lat_lon))
+        # prediction = df_classification_y_real_values.squeeze().to_list()[int_prediction]
+        prediction = str(capacity_mw)
+    
     return prediction
+    
 
 if __name__ == "__main__":
     app.run(port=8080)
